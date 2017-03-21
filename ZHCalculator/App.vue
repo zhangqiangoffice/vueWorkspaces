21e10dfca3d8d54b8e20cc154fff343906726816
zhangqiangoffice <zhangqiangoffice@163.com>
2017-03-21 23:53:10 +0800

    cif json

---
+++ ZHCalculator/App.vue
@@ -0,0 +1,78 @@
+<template>
+  <div id="app">
+    <ul>
+      <li>
+        <span>性别</span>
+        <br>
+        <input type="radio" id="nan" value="nan" v-model="sex">
+        <label for="nan">男</label>
+        <br>
+        <input type="radio" id="nv" value="nv" v-model="sex">
+        <label for="nv">女</label>
+        <br>
+        <span>Picked: {{ sex }}</span>
+      </li>
+
+      <li>
+        <span>年期</span>
+        <br>
+        <input type="radio" id="ten_years" value="10" v-model="year">
+        <label for="ten_years">十年</label>
+        <br>
+        <input type="radio" id="twenty_years" value="20" v-model="year">
+        <label for="twenty_years">二十年</label>
+        <br>
+        <span>Picked: {{ year }}</span>
+      </li>
+
+      <li>
+        <span>年龄</span>
+        <br>
+        <input v-model="age" placeholder="投保年龄">
+        <br>
+        <span>Input: {{ age }}</span>
+      </li>
+      
+      <li>
+        <span>费率</span>
+        <br>
+        <span>Input: {{ rate }}</span>
+      </li>
+
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      sex: 'nan',
+      year: 10,
+      age: 18,
+      rate: 2024,
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+ul {
+  list-style-type: none;
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+}
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import App from './App.vue'
+import data from './cif.json'
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
