4f2c2ad86b6d9f3b0b6fc615f93e102ccf72cfaf
zhangqiang office <zhangqiangoffice@163.com>
2017-03-23 15:15:50 +0800

    hrc and hrd

--- ZHCalculator/App.vue
+++ ZHCalculator/App.vue
@@ -1,8 +1,6 @@
-
 <template>
   <div id="app">
     <ul>
-      <li>CIF主险</li>
       <li>
         <span>性别</span>
         <br>
@@ -11,16 +9,12 @@
         <br>
         <input type="radio" id="nv" value="nv" v-model="sex">
         <label for="nv">女</label>
-        <br>
-        <span>Picked: {{ sex }}</span>
       </li>
 
       <li>
         <span>年龄</span>
         <br>
         <input type="number" class="short" v-model="age" placeholder="投保年龄">
-        <br>
-        <span>Input: {{ age }} 岁</span>
       </li>
 
       <li>
@@ -36,10 +30,8 @@
             {{ job.work }}
           </option>
         </select>
-        <br>
-        <span>Selected: {{ occupation.work }}</span>
       </li>
-
+    
       <li>
         <span>年期</span>
         <br>
@@ -48,37 +40,54 @@
         <br>
         <input type="radio" id="twenty_years" value="20" v-model="year">
         <label for="twenty_years">二十年</label>
-        <br>
-        <span>Picked: {{ year }}</span>
       </li>
 
       <li>
         <span>保额(万元)</span>
         <br>
         <input type="number" class="short" v-model="coverage" placeholder="投保年龄">
-        <br>
-        <span>Input: {{ coverage }} 万元</span>
       </li>
       
       <li>
-        <span>年缴保费</span>
+        <span>CIF主险</span>
         <br>
-        <span>Result: {{ cifFee | capitalize }}</span>
+        <span>年缴保费: {{ cifFee | capitalize }}</span>
       </li>
     </ul>
 
     <ul>
-      <li>HRC或HRD</li>
       <li>
-        <span>是否购买社保或新农合</span>
+        <span>已购买社保或新农合</span>
         <br>
-        <input type="radio" id="hasSocialSecurityTrue" value="true" v-model="hasSocialSecurity">
-        <label for="hasSocialSecurityTrue">是</label>
+        <input type="checkbox" id="hasSocialSecurity" v-model="hasSocialSecurity">
+        <label for="hasSocialSecurity">{{ hasSocialSecurity | yesOrNo }}</label>
+      </li>
+
+      <li>
+        <span>首次投保</span>
         <br>
-        <input type="radio" id="hasSocialSecurityFlse" value="false" v-model="hasSocialSecurity">
-        <label for="hasSocialSecurityFlse">否</label>
+        <input type="checkbox" id="isFirst" v-model="isFirst">
+        <label for="isFirst">{{ isFirst | yesOrNo }}</label>
+      </li>
+
+      <li>
+        <span>投保计划</span>
         <br>
-        <span>Picked: {{ hasSocialSecurity }}</span>
+        <select v-model="plan" class="middle">
+          <option v-for="num in planTop" v-bind:value="num">
+            计划{{ chars[num] }}
+          </option>
+        </select>
+      </li>
+      <li v-show="!hasSocialSecurity">
+        <span>HRC</span>
+        <br>
+        <span>附加保费: {{ hrcFee | capitalize }}</span>
+      </li>
+      <li v-show="hasSocialSecurity">
+        <span>HRD</span>
+        <br>
+        <span>附加保费: {{ hrdFee | capitalize }}</span>
       </li>
     </ul>
   </div>
@@ -86,6 +95,8 @@
 
 <script>
 import cifRates from './cif.json';
+import hrcRates from './hrc.json';
+import hrdRates from './hrd.json';
 import industries from './industryCategory.json';
 export default {
   name: 'app',
@@ -95,8 +106,11 @@ export default {
       year: 10,
       age: 18,
       hasSocialSecurity: false,
+      isFirst: true,
       coverage: 10,
       indu: '农牧业 - 农业',
+      chars: ['零', '一', '二', '三', '四', '五', '六'],
+      plan: 1,
       occupation: {"industory": "农牧业 - 农业", "code": "02060", "work": "农民", "life": 0, "accident": 2, "hospital": 2}
     }
   },
@@ -107,12 +121,25 @@ export default {
         return '不可投保！'
       }
       return val.toFixed(2) + ' 元';
+    },
+    //判断：“是”或者“否”
+    yesOrNo: function(val) {
+      return val ? '是' : '否'; 
     }
+
   },
   computed: {
     //CIF主险保费
     cifFee: function () {
       return (cifRates[`${this.sex}_year${this.year}_age${this.age}`] + this.occupation.life) * this.coverage;
+    },
+    //hrc附加保费
+    hrcFee: function () {
+      return Math.round(hrcRates[`occu${this.occupation.hospital}_group${this.ageGroup}_plan${this.plan}`] * (this.isFirst ? 0.95 : 1))
+    },
+    //hrd附加保费
+    hrdFee: function () {
+      return Math.round(hrdRates[`occu${this.occupation.hospital}_group${this.ageGroup}_plan${this.plan}`] * (this.isFirst ? 0.95 : 1))
     },
     //所有行业的名称数组
     indus: function() {
@@ -125,12 +152,24 @@ export default {
       }
       return industriesArr;
     },
+    //年龄段
+    ageGroup: function() {
+      if (this.age <= 17) {
+        return parseInt(this.age / 5);
+      } else {
+        return parseInt(this.age / 5) + 1;
+      }
+    },
     //选择行业后的所有工种
     jobs: function () {
       const that = this;
       return industries.filter(function(ele) {
         return ele.industory === that.indu;
       })
+    },
+    //最高可以购买的投保计划等级
+    planTop: function() {
+      return this.ageGroup < 4 ? 3 : 6;
     }
   }
 }
