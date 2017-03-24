281edc12ef93560916e90b133146c4859f4a4719
zhangqiang office <zhangqiangoffice@163.com>
2017-03-24 15:24:01 +0800

    clauses

--- ZHCalculator/App.vue
+++ ZHCalculator/App.vue
@@ -2,11 +2,13 @@
   <div id="app">
     <ul>
       <li>
+        被保险人
+      </li>
+      <li>
         <span>性别</span>
         <br>
         <input type="radio" id="nan" value="nan" v-model="sex">
         <label for="nan">男</label>
-        <br>
         <input type="radio" id="nv" value="nv" v-model="sex">
         <label for="nv">女</label>
       </li>
@@ -14,7 +16,7 @@
       <li>
         <span>年龄</span>
         <br>
-        <input type="number" class="short" v-model="age" placeholder="投保年龄">
+        <input type="number" class="short" :min="0" :max="50" v-model.number="age" >
       </li>
 
       <li>
@@ -25,6 +27,7 @@
             {{ ind }}
           </option>
         </select>
+        <br>
         <select v-model="occupation" class="middle">
           <option v-for="job in jobs" v-bind:value="job">
             {{ job.work }}
@@ -32,30 +35,32 @@
         </select>
       </li>
     
+    </ul>
+    <ul>
+      <li>CIF 主险</li>
       <li>
         <span>年期</span>
         <br>
-        <input type="radio" id="ten_years" value="10" v-model="year">
+        <input type="radio" id="ten_years" value="10" v-model.number="year">
         <label for="ten_years">十年</label>
-        <br>
-        <input type="radio" id="twenty_years" value="20" v-model="year">
+        <input type="radio" id="twenty_years" value="20" v-model.number="year">
         <label for="twenty_years">二十年</label>
       </li>
-
       <li>
         <span>保额(万元)</span>
         <br>
-        <input type="number" class="short" v-model="coverage" placeholder="投保年龄">
+        <input type="number" class="short" :min="1" :max="50" v-model.number="coverage" >
       </li>
       
-      <li>
+      <li class="fee">
         <span>CIF主险</span>
         <br>
-        <span>年缴保费: {{ cifFee | capitalize }}</span>
+        <span>年缴保费: <span :class="getColorClass(cifFee)">{{ cifFee | capitalize }}</span></span>
       </li>
     </ul>
 
     <ul>
+      <li>HRC或HRD</li>
       <li>
         <span>已购买社保或新农合</span>
         <br>
@@ -79,99 +84,100 @@
           </option>
         </select>
       </li>
-      <li v-show="!hasSocialSecurity">
+      <li v-show="!hasSocialSecurity" class="fee">
         <span>HRC</span>
         <br>
-        <span>附加保费: {{ hrcFee | capitalize }}</span>
+        <span>附加保费: <span :class="getColorClass(hrcFee)">{{ hrcFee | capitalize }}</span></span>
       </li>
-      <li v-show="hasSocialSecurity">
+      <li v-show="hasSocialSecurity" class="fee">
         <span>HRD</span>
         <br>
-        <span>附加保费: {{ hrdFee | capitalize }}</span>
+        <span>附加保费: <span :class="getColorClass(hrdFee)">{{ hrdFee | capitalize }}</span></span>
       </li>
     </ul>
 
     <ul>
+      <li>ADDC</li>
       <li>
         <span>ADDC保额(万元)</span>
         <br>
-        <input type="number" class="short" v-model="addcCoverage">
+        <input type="number" class="short" :min="0" :max="99" v-model.number="addcCoverage">
       </li>
-      <li>
+      <li class="fee">
         <span>ADDC</span>
         <br>
-        <span>附加保费: {{ addcFee | capitalize }}</span>
+        <span>附加保费: <span :class="getColorClass(addcFee)">{{ addcFee | capitalize }}</span></span>
       </li>
     </ul>
 
-    <ul v-show="addcFee > 0">
+    <ul>
+      <li>AMRC</li>
       <li>
         <span>AMRC保额(千元)</span>
         <br>
-        <input type="number" class="short" v-model="amrcCoverage">
+        <input type="number" class="short" :min="0" v-model.number="amrcCoverage">
       </li>
-      <li>
+      <li class="fee">
         <span>AMRC</span>
         <br>
-        <span>附加保费: {{ amrcFee | capitalize }}</span>
+        <span>附加保费: <span :class="getColorClass(amrcFee)">{{ amrcFee | capitalize }}</span></span>
       </li>
     </ul>
 
     <ul >
+      <li>投保人</li>
       <li>
         <span>投保人是被保人的</span>
         <br>
         <input type="radio" id="self" value="self" v-model="relation">
         <label for="self">自己</label>
-        <br>
         <input type="radio" id="parent" value="parent" v-model="relation">
         <label for="parent">父母</label>
-        <br>
         <input type="radio" id="other" value="other" v-model="relation">
         <label for="other">其他</label>
       </li>
       <li>
         <span>投保人年龄</span>
         <br>
-        <input type="number" class="short" v-model="applicantAge" >
+        <input type="number" class="short" :min="0" v-model.number="applicantAge" >
       </li>
       <li>
         <span>投保人性别</span>
         <br>
         <input type="radio" id="applicantNan" value="nan" v-model="applicantSex">
         <label for="applicantNan">男</label>
-        <br>
         <input type="radio" id="applicantNv" value="nv" v-model="applicantSex">
         <label for="applicantNv">女</label>
       </li>
 
-      <li>
-        <span>保费合计</span>
-        <br>
-        <span>{{ totalFee | capitalize}}</span>
-      </li>
     </ul>
 
     <ul>
+      <li>W系列</li>
       <li>
+        <span>主险及附加保费合计</span>
+        <br>
+        <span>{{ totalFee | capitalize}}</span>
+      </li>
+      <li class="fee">
         <span>WA</span>
         <br>
-        <span>附加保费: {{ waFee | capitalize }}</span>
+        <span>附加保费: <span :class="getColorClass(waFee)">{{ waFee | capitalize }}</span></span>
       </li>
-      <li>
+      <li class="fee">
         <span>WPA</span>
         <br>
-        <span>附加保费: {{ wpaFee | capitalize }}</span>
+        <span>附加保费: <span :class="getColorClass(wpaFee)">{{ wpaFee | capitalize }}</span></span>
       </li>
-      <li>
+      <li class="fee">
         <span>WPB</span>
         <br>
-        <span>附加保费: {{ wpbFee | capitalize }}</span>
+        <span>附加保费: <span :class="getColorClass(wpbFee)">{{ wpbFee | capitalize }}</span></span>
       </li>
-      <li>
+      <li class="fee">
         <span>WP</span>
         <br>
-        <span>附加保费: {{ wpFee | capitalize }}</span>
+        <span>附加保费: <span :class="getColorClass(wpFee)">{{ wpFee | capitalize }}</span></span>
       </li>
     </ul>
 
@@ -215,16 +221,24 @@ export default {
   filters: {
     //数字金额化
     capitalize: function(val) {
-      if (isNaN(val)) {
+      
+      //字符串提示
+      if ( (typeof val === 'string') && val.constructor === String) {
+        return val;
+      }
+
+      //NaN或负数
+      if (isNaN(val) || val < 0) {
         return '不可投保！'
       }
+
+      //金额
       return val.toFixed(2) + ' 元';
     },
     //判断：“是”或者“否”
     yesOrNo: function(val) {
       return val ? '是' : '否'; 
-    }
-
+    },
   },
   computed: {
     //CIF主险保费
@@ -233,34 +247,91 @@ export default {
     },
     //hrc附加保费
     hrcFee: function () {
+      
+      //主险必须投保成功
+      if (this.noFee(this.cifFee) ) {
+        return '未买主险'
+      }
+
       return hrcRates[`occu${this.occupation.hospital}_group${this.ageGroup}_plan${this.plan}`] * (this.isFirst ? 0.95 : 1)
     },
     //hrd附加保费
     hrdFee: function () {
+      
+      //主险必须投保成功
+      if (this.noFee(this.cifFee) ) {
+        return '未买主险'
+      }
+
       return hrdRates[`occu${this.occupation.hospital}_group${this.ageGroup}_plan${this.plan}`] * (this.isFirst ? 0.95 : 1)
     },
     //addc附加保费
     addcFee: function () {
+      
+      //主险必须投保成功
+      if (this.noFee(this.cifFee) ) {
+        return '未买主险'
+      }
+
       return addcRates[`occu${this.occupation.accident}`] * 10 * this.addcCoverage
     },
     //amrc附加保费
     amrcFee: function () {
+      
+      //主险必须投保成功
+      if (this.noFee(this.cifFee) ) {
+        return '未买主险'
+      }
+
+      //ADDC必须投保成功
+      if (this.noFee(this.addcFee) ) {
+        return '未买ADDC'
+      }
+
       return amrcRates[`occu${this.occupation.accident}`] * 1 * this.amrcCoverage
     },
     //wa附加保费
     waFee: function () {
+      
+      //主险必须投保成功
+      if (this.noFee(this.cifFee) ) {
+        return '未买主险'
+      }
+
       return waRates[`${this.sex}_year${this.year}_age${this.age}`] * 1 * (this.totalFee / 1000)
     },
     //wpa附加保费
     wpaFee: function () {
+      
+      //主险必须投保成功
+      if (this.noFee(this.cifFee) ) {
+        return '未买主险'
+      }
+
       return wpaRates[`age${this.applicantAge}_year${this.year}`] * 1 * (this.totalFee / 1000)
     },
     //wpb附加保费
     wpbFee: function () {
+      
+      //主险必须投保成功
+      if (this.noFee(this.cifFee) ) {
+        return '未买主险'
+      }
+
       return wpbRates[`${this.applicantSex}_year${this.year}_age${this.applicantAge}`] * 1 * (this.totalFee / 1000)
     },
     //wp附加保费
     wpFee: function () {
+      
+      //主险必须投保成功
+      if (this.noFee(this.cifFee) ) {
+        return '未买主险'
+      }
+
+      //投被保人为同一人时才可投保wp
+      if (this.relation !== 'self') {
+        return '需为同一人'
+      }
       return wpRates[`age${this.applicantAge}_year${this.year}`] * 1 * (this.totalFee / 1000)
     },
 
@@ -315,6 +386,14 @@ export default {
     planTop: function() {
       return this.ageGroup < 4 ? 4 : 7;
     }
+  },
+  methods: {
+    getColorClass: function(val) {
+      return val > 0 ? 'ok' : 'no'
+    },
+    noFee: function (fee) {
+      return ! (fee > 0)
+    }
   }
 }
 </script>
@@ -346,6 +425,27 @@ ul {
     display: inline-block;
     margin: 0 10px;
     vertical-align: top;
+
+    &:first-child{
+      font-size: 20px;
+      float: left;
+      color: orange;
+    }
+
+    &.fee {
+      color: sienna;
+      float: right;
+      width: 165px;
+      text-align: left;
+
+      .ok {
+        color: green
+      }
+
+      .no {
+        color: red;
+      }
+    }
   }
 }
 
